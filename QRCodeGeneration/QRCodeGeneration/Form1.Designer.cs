f9fcb42d5fcf63d9eb9d36bf207e539e2f6a0651
Võ Hồng Quân <71002261+vohongquan9998@users.noreply.github.com>
2021-12-19 19:42:15 +0700

    Add files via upload

---
+++ QRCodeGeneration/QRCodeGeneration/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QRCodeGeneration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picQRCode = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQRCode
+            // 
+            this.picQRCode.Location = new System.Drawing.Point(12, 50);
+            this.picQRCode.Name = "picQRCode";
+            this.picQRCode.Size = new System.Drawing.Size(350, 300);
+            this.picQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQRCode.TabIndex = 0;
+            this.picQRCode.TabStop = false;
+            this.picQRCode.Click += new System.EventHandler(this.picQRCode_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "QR Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Text";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(16, 385);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(347, 77);
+            this.txtText.TabIndex = 2;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(141, 471);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(105, 41);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 519);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picQRCode);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code";
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picQRCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ QRCodeGeneration/QRCodeGeneration/Form1.cs
@@ -0,0 +1,38 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeGeneration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qR = new QRCodeGenerator();
+            QRCodeData data = qR.CreateQrCode(txtText.Text,QRCodeGenerator.ECCLevel.Q);
+            QRCode rCode = new QRCode(data);
+            picQRCode.Image = rCode.GetGraphic(5);
+        }
+
+        private void picQRCode_Click(object sender, EventArgs e)
+        {
+            if(picQRCode.Image!=null)
+            {
+                Process.Start(txtText.Text);
+            }
+        }
+    }
+}
